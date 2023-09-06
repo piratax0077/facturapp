e89bd8216920dda56ab58835c0113b8cc981818a
piratax0077 <francisco.rojo.gallardo@gmail.com>
2023-09-06 13:00:39 -0300

    se busca repuesto con ruta api a panchoserver

--- public/js/repuestos.js
+++ public/js/repuestos.js
@@ -2,3 +2,32 @@ window.onload = function () {
     // mostrar modal para buscar repuestos
     $('#modalBuscarRepuesto').modal('show');
 };
+
+function buscarRepuesto(){
+    let busqueda = document.getElementById('busqueda').value;
+    fetch("https://panchoserver.ddns.net/api/"+busqueda+"/buscadordemo")
+    .then(res => res.json())
+    .then(data => {
+        let repuestos = data[0];
+        console.log(repuestos);
+        let html = '';
+        repuestos.forEach(repuesto => {
+            html += `
+                <div class="col-md-3">
+                <div class="card mt-4 text-center">
+                    <div class="card-header">
+                        <h5>${repuesto.descripcion}</h5>
+                    </div>
+                    <div class="card-body">
+                        <img src="https://panchoserver.ddns.net/storage/${repuesto.urlfoto}" alt="" class="img-fluid">
+                    </div>
+                    <div class="card-footer">
+                        <button class="btn btn-success btn-sm" onclick="paciencia()">Agregar </button>
+                    </div>
+                </div>
+                </div>
+            `
+        });
+        document.getElementById('resultado').innerHTML = html;
+    });
+}
